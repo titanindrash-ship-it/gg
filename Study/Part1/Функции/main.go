f86b5bc93d36ac1e660eb2f56e5f123e860d7fce
titanindrash-ship-it <titan.indrash@gmail.com>
2025-10-07 16:18:46 +0300

    Add files via upload

--- Study/Part1/Функции/main.go
+++ Study/Part1/Функции/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	name("Василий")
+	name("Дмитрий")
+	name("Антон")
+	name("Евгений")
+}
+func name(name string) {
+	fmt.Println("Добрый день, уважаемый", name)
+	fmt.Println("Проходите за данный столик")
+	fmt.Println("Вот ваше меню, как будете готоы сделать заказ, нажмите на кнопку")
+	fmt.Println("----------------------------------------")
+	time.Sleep(500 * time.Millisecond)
+}
